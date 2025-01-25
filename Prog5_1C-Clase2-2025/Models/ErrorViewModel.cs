4b2a282fd4984d92cd304948d6f0cafad8260d2a
nayahi <nayahi@hotmail.com>
2025-01-24 20:26:30 -0600

    Add project files.

---
+++ Prog5_1C-Clase2-2025/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Prog5_1C_Clase2_2025.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
